9a983aab3f863d3fe3eb381f3387bc774348ebda
Matt Ramos <1260952+mttrms@users.noreply.github.com>
2021-07-31 17:14:09 -0400

    Make emoji accessible to fix compile (#16)

--- src/results/index.jsx
+++ src/results/index.jsx
@@ -4,7 +4,7 @@ const Results = ({runningTime, letterSplits}) => {
   const splits = Object.keys(letterSplits).map(char => <li key={char}><span className="char">{char}:</span> {letterSplits[char]}s</li>)
   return (
     <div className="results">
-      <p>Detailed Results 🏁</p>
+      <p>Detailed Results <span role="img" aria-label="flag">🏁</span></p>
       <ul>
         { splits }
       </ul>
